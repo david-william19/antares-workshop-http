22c20164e4df46702d4df79ef91ae48f77a9916c
davidwilliam <davidddwlliam@gmail.com>
2023-01-17 10:22:03 +0700

    fill getLatestData, temperature and updateContent

--- service/antaresAPI.js
+++ service/antaresAPI.js
@@ -1,19 +1,7 @@
 const axios = require("axios");
 
 const getLatestData = async () => {
-  try {
-    const url = `https://platform.antares.id:8443/~/antares-cse/antares-id/${process.env.applicationName}/${process.env.deviceName}/la`;
-    const result = await axios.get(url, {
-      headers: {
-        "X-M2M-Origin": process.env.accessKey,
-        Accept: "application/json",
-        "Content-Type": "application/json;ty=4",
-      },
-    });
-    return result.data;
-  } catch (e) {
-    throw new Error("Error");
-  }
+  // isi fungsi getLatestData dibawah
 };
 
 const getDataAntaresID = async () => {
